6fddf6846fe8f9f518e60b309222599cb1e58d0e
Rajeshwaran <rajeshfederer2@gmail.com>
2018-03-23 14:33:01 +0530

    getPolicyInfo

--- app.js
+++ app.js
@@ -11,33 +11,45 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
 app.post('/', (req, res) =>{
-    let resObj =  {
-        "speech": "Hi Fredrick Gonsalves, Here are your existing Portfolio details for Variable Annuity Policy No $policyInfo.original",
-        "messages": [{
+  if(req.body.result.action == "getPolicyInfo"){
+    let policyNo;
+    let parameters = element.parameters;
+    if(parameters.policyInfo){
+      policyNo = parameters.policyInfo;
+    }
+    let resMsg = {
+      speech: '',
+      messages: [
+        {
           "type": 0,
           "platform": "facebook",
-          "speech": "Hi Fredrick Gonsalves, Here are your existing Portfolio details for Variable Annuity Policy No $policyInfo.original"
+          "speech": "Hi Fredrick Gonsalves, Here are your existing Portfolio details for variable annuity policy No "+policyNo + "\n\n Alamere Equity Income\n Alamere Money Market. \n"
         },
-        {
-          "type": 2,
-          "platform": "facebook",
-          "text": "Would you like me to help you with your investment planning?",
-    "quick_replies": [
-      {
-        "content_type": "text",
-        "title": "Yes",
-        "payload": "Yes"
-      },
-      {
-        "content_type": "text",
-        "title": "No",
-        "payload": "No"
-      }
-    ]
-        }]
-    };
-    console.log('IN', req.body);
-    return res.json(resObj);
+          {
+            "type": 2,
+            "platform": "facebook",
+            "payload":{
+              "facebook": {
+                "text": "Would you like me to help you with your investment planning?",
+                "quick_replies": [
+                  {
+                    "content_type": "text",
+                    "title": "Yes",
+                    "payload": "Yes"
+                  },
+                  {
+                    "content_type": "text",
+                    "title": "No",
+                    "payload": "No"
+                  }
+                ]
+              }
+            }
+          }
+        ]
+  };
+    return res.json(resMsg);
+  }
 });
 
 app.listen(port, function(){
