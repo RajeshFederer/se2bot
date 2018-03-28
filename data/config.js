c1e2cc9635472201cdcd46c2aafb7f1666731926
Rajeshwaran <rajeshfederer2@gmail.com>
2018-03-28 17:04:28 +0530

    added user config

---
+++ data/config.js
@@ -0,0 +1,64 @@
+module.exports = {
+
+    clientDetails: [{
+        "FirstName": "Fredrick",
+        "LastName": "Gonsalves",
+        "MaritalStatus": "Married",
+        "DOB": "12-10-1960",
+        "AnnualEarnedIncome": 250000,
+        "TotalNetWorth": 90000000,
+        "LegalResidenceAddress": "141,First Avenue",
+        "City": "Newyork",
+        "Zip": 8795465,
+        "Employer": "Priverica Corp",
+        "Occupation": "Consultant",
+        "TaxId": 986574,
+        "policyDetails": {
+            "PolicyNumber": "ABDULA0001",
+            "PolicyStatus": "Active",
+            "ContractIssueDate": "10-10-2010",
+            "OwnerAgeAtIssue": 50
+        },
+        "fundDetails" :[{
+            "fundName": "Alamere Equity Income",
+            "CashValue": "10,842.95 USD",
+            "Units" : 996.831411,
+            "UnitValue" : 10.87741207
+        },{
+            "fundName": "Alamere Money Market",
+            "CashValue": "9,678.54 USD",
+            "Units" : 889.783484,
+            "UnitValue" : 10.87741207
+        }]
+    }, {
+        "FirstName": "Rajesh",
+        "LastName": "waran",
+        "MaritalStatus": "Married",
+        "DOB": "12-10-1960",
+        "AnnualEarnedIncome": 250000,
+        "TotalNetWorth": 90000000,
+        "LegalResidenceAddress": "141,First Avenue",
+        "City": "Newyork",
+        "Zip": 8795465,
+        "Employer": "Priverica Corp",
+        "Occupation": "Consultant",
+        "TaxId": 986574,
+        "policyDetails": {
+            "PolicyNumber": "ABDULA0002",
+            "PolicyStatus": "Active",
+            "ContractIssueDate": "10-10-2010",
+            "OwnerAgeAtIssue": 50
+        },
+        "fundDetails" :[{
+            "fundName": "Alamere Equity Income",
+            "CashValue": "10,842.95 USD",
+            "Units" : 996.831411,
+            "UnitValue" : 10.87741207
+        },{
+            "fundName": "Alamere Money Market",
+            "CashValue": "9,678.54 USD",
+            "Units" : 889.783484,
+            "UnitValue" : 10.87741207
+        }]
+    }]
+};
