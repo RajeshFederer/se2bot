71ef0dc57e54725547349b0839d0c99fa26b1656
Rajeshwaran <rajeshfederer2@gmail.com>
2018-03-28 16:57:35 +0530

    action changes and adding config

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
+let action = require('./lib/action');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -11,46 +12,7 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
 app.post('/', (req, res) =>{
-  if(req.body.result.action == "getPolicyInfo"){
-    console.log('IN GETPOLICYINFO');
-    let policyNo;
-    let parameters = req.body.result.parameters;
-    if(parameters.policyInfo){
-      policyNo = parameters.policyInfo;
-    }
-    let resMsg = {
-      speech: '',
-      messages: [
-        {
-          "type": 0,
-          "platform": "facebook",
-          "speech": "Hi Fredrick Gonsalves, Here are your existing Portfolio details for variable annuity policy No "+policyNo + " </br></br> Alamere Equity Income</br> Alamere Money Market."
-        },
-          {
-            "type": 4,
-            "platform": "facebook",
-            "payload":{
-              "facebook": {
-                "text": "Would you like me to help you with your investment planning?",
-                "quick_replies": [
-                  {
-                    "content_type": "text",
-                    "title": "Yes",
-                    "payload": "Yes"
-                  },
-                  {
-                    "content_type": "text",
-                    "title": "No",
-                    "payload": "No"
-                  }
-                ]
-              }
-            }
-          }
-      ]
-  };
-    return res.json(resMsg);
-  }
+  return action[req.body.result.action](req, res);
 });
 
 app.listen(port, function(){
