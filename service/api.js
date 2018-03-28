e714da73ce77335094d66d930c58305ece8302b7
Rajeshwaran <rajeshfederer2@gmail.com>
2018-03-28 19:25:53 +0530

    api service added

---
+++ service/api.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const request = require('request');
+
+module.exports.callApi = (url, method, body) =>{
+
+    let options = {
+        method : method,
+        url: url,
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept':'application/json'
+        },
+        json : true
+    };
+    if (method == "POST" && body){
+        options.body = body;
+    }
+    
+    return new Promise((resolve, reject) =>{
+        request(options, function (err, response, body) {
+            console.log('RESPONSE', err, body);
+            if (err){
+                reject(err);
+            } else {
+                resolve(body);
+            }
+        });
+    });
+};
