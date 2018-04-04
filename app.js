eb875d5e5fad6b84ae7c1618c66be3908248e54d
Rajeshwaran <rajeshfederer2@gmail.com>
2018-04-04 14:14:59 +0530

    session implementation

--- app.js
+++ app.js
@@ -4,27 +4,37 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
 const path = require('path');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+
 let action = require('./lib/action');
 
 const app = express();
+
 const port = process.env.PORT || 3000;
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: true
+}));
 
-app.post('/', (req, res) =>{
-  console.log('ACTION NAME ',req.body.result.action);
-  if(req.body.result.action =="getPolicyInvalid"){
-    return action["getPolicyInfo"](req, res);  
-  } else{
+app.post('/', (req, res) => {
+  console.log('ACTION NAME ', req.body.result.action);
+  if (req.body.result.action == "getPolicyInvalid") {
+    return action["getPolicyInfo"](req, res);
+  } else {
     return action[req.body.result.action](req, res);
   }
 });
 
-app.get('/image/presentMarket',function(req,res){
+app.get('/image/presentMarket', function (req, res) {
   res.sendFile(path.join(__dirname, '/images/presentMarket.jpg'));
 });
 
-app.listen(port, function(){
-    console.log('AGENT is running my app on  PORT: ' + port);
+app.listen(port, function () {
+  console.log('AGENT is running my app on  PORT: ' + port);
 });
