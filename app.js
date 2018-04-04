5660065f10275a52e98c26fb8eaa91be1e8fbc28
Rajeshwaran <rajeshfederer2@gmail.com>
2018-04-04 09:51:24 +0530

    data and alignment

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 
 app.post('/', (req, res) =>{
-  console.log('USER:'+req.body.result.resolvedQuery);
   if(req.body.result.action =="getPolicyInvalid"){
     return action["getPolicyInfo"](req, res);  
   } else{
